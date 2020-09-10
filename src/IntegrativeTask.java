ed3aba1535aad8b96692644b102a322f0318ce9c
CamiloCJ09 <camilocampazj@gmail.com>
2020-09-10 16:33:29 -0500

    Trabajo

---
+++ src/IntegrativeTask.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IntegrativeTask {
+    public static void main(String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int countOfItems = 0;
+        String [] nameOfMaterials = new String[0];
+        double [] amountOfMaterials = new double[0];
+        System.out.println("Hola");
+        
+    }
+}
