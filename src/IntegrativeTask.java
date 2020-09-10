b551932d768911a2434412e8494495ba417e1a7e
CamiloCJ09 <camilocampazj@gmail.com>
2020-09-10 17:12:12 -0500

    Work 1

--- src/IntegrativeTask.java
+++ src/IntegrativeTask.java
@@ -1,12 +1,26 @@
 import java.util.Scanner;
 public class IntegrativeTask {
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int countOfItems = 0;
-        String [] nameOfMaterials = new String[0];
-        double [] amountOfMaterials = new double[0];
-        System.out.println("Hola");
+        int x = 0;
+        int countOfItems = AskCountArticles(x);
+        String [] nameOfMaterials = new String[x];
+        double [] amountOfMaterials = new double[x];
+        double [] priceOfMaterialsHomecenter = new double[x];
+        double [] priceOfMaterialsDownTownGrocery = new double[x];
+        double [] priceOfMaterialsNeihborhood = new double[x];
+   
+    }
+    public static int AskCountArticles(int countOfItems) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How much materials do you want to buy?");
+        countOfItems = sc.nextInt();
+        return countOfItems;
+    }
+    public static String [] AssingValues(String [] nameOfMaterials){
         
+        return nameOfMaterials;
     }
 }
