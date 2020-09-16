0975562bcc8c71b1146a74096564b667595aa82e
CamiloCJ09 <camilocampazj@gmail.com>
2020-09-15 23:05:09 -0500

    Final Stage

---
+++ README.md
@@ -0,0 +1,10 @@
+# Integrative task
+
+Change logs:
+
+## 10/09/2020
+
+<ul>
+    <li>
+    </li>
+</ul>
--- src/IntegrativeTask.java
+++ src/IntegrativeTask.java
@@ -11,6 +11,9 @@ public class IntegrativeTask {
         double [] priceOfMaterialsHomecenter = new double[x];
         double [] priceOfMaterialsDownTownGrocery = new double[x];
         double [] priceOfMaterialsNeihborhood = new double[x];
+        double sumHomecenter = 0;
+        double sumDownTown = 0;
+        double sumNeighboor = 0;
 
         //Calls
         Object [] array = new Object[x]; //We use this array to say the type of work
@@ -18,16 +21,36 @@ public class IntegrativeTask {
         nameOfMaterials = NameOfObjets(nameOfMaterials, x);
         amountOfMaterials = AssingAmount(amountOfMaterials, nameOfMaterials, x);
         array = TypeOfWorks(array,nameOfMaterials,x);
-        priceOfMaterialsHomecenter = AssingPricesHomecenter(amountOfMaterials, nameOfMaterials, x);
-        priceOfMaterialsDownTownGrocery = AssingPricesDownTownGrocery(amountOfMaterials, nameOfMaterials, x);
-        priceOfMaterialsNeihborhood = AssingPricesNehiborhoodStore(amountOfMaterials, nameOfMaterials, x);
+        priceOfMaterialsHomecenter = AssingPricesHomecenter(priceOfMaterialsHomecenter, nameOfMaterials, x);
+        priceOfMaterialsDownTownGrocery = AssingPricesDownTownGrocery(priceOfMaterialsDownTownGrocery, nameOfMaterials, x);
+        priceOfMaterialsNeihborhood = AssingPricesNehiborhoodStore(priceOfMaterialsNeihborhood, nameOfMaterials, x);
 
 
-        /*for(int i = 0; i<x;i++){
-            System.out.println(nameOfMaterials[i]); //Imprimir los el tipo de obra que va a ser
+        for(int i = 0; i<x;i++){
+            System.out.println(priceOfMaterialsHomecenter[i]); //Imprimir los el tipo de obra que va a ser
         }
-        */
+        for(int j = 0; j<x;j++){
+            System.out.println(priceOfMaterialsDownTownGrocery[j]); //Imprimir los el tipo de obra que va a ser
+        }
+        for(int k = 0; k<x;k++){
+            System.out.println(priceOfMaterialsNeihborhood[k]); //Imprimir los el tipo de obra que va a ser
+        }
+        sumHomecenter = SumPerStore(nameOfMaterials, priceOfMaterialsHomecenter, amountOfMaterials, array, x);
+        sumDownTown = SumPerStore(nameOfMaterials, priceOfMaterialsDownTownGrocery, amountOfMaterials, array, x);
+        sumNeighboor = SumPerStore(nameOfMaterials, priceOfMaterialsNeihborhood, amountOfMaterials, array, x);
+        System.out.println("The sum of materials in Homecenter is: "+sumHomecenter+"\n"+"The sum of materials in DownTown Grocery is: "+sumDownTown+"\n"+"The sum of materials in Neihgborhood store is: "+sumNeighboor);
 
+        WhereisBetterToBuy(priceOfMaterialsHomecenter, priceOfMaterialsDownTownGrocery, priceOfMaterialsNeihborhood, x, nameOfMaterials);
+        
+        System.out.println("the materials of which work you want to know");
+        int deploy = sc.nextInt();
+        String [] namesPerType = deployMaterials(array, nameOfMaterials, x, deploy);
+        for(int i = 0; i<x;i++){
+            if(namesPerType[i]!=null){
+                System.out.println(namesPerType[i]);
+            }
+        }
+        
     }
 
     public static int AskCountArticles() {
@@ -97,5 +120,83 @@ public class IntegrativeTask {
             i++;
         }
         return theTypeOfWork;
+    }
+    public static double SumPerStore(String [] nameOfMaterials,double [] imputMaterial,double [] amountOfMaterials,Object [] typeObject, int x){
+        double valueFromStore = 0;
+        int white = 0;
+        int black = 0;
+        int paint = 0;
+        double imptmaterial = 0;
+        double amntMaterial = 0;
+        double [] array = new double[x];
+        for(int i = 0; i<x; i++){
+            System.out.println(nameOfMaterials[i]);
+            imptmaterial = imputMaterial[i];
+            amntMaterial = amountOfMaterials[i];
+            
+            array[i] = imptmaterial*amntMaterial; //Comprobación de funcionamiento
+            System.out.println("A: "+amountOfMaterials[i]);
+            System.out.println("B: "+imputMaterial[i]);
+            System.out.println("T: "+array[i]);
+        }
+        for(int j = 0; j<x; j++){
+            valueFromStore += array[j];
+        }
+        for(int k = 0; k<x;k++){
+            if(typeObject[k]==TypeOfWork.BLACK){
+                black++;
+            }
+            if(typeObject[k]==TypeOfWork.WHITE){
+                white++;
+            }
+            if(typeObject[k]==TypeOfWork.PAINT){
+                paint++;
+            }
+        }
+        System.out.println(valueFromStore);
+        if(black>0){
+            valueFromStore += 1300000;
+        }
+        if(white>0){
+            valueFromStore += 2600000;
+        }
+        if(paint>0){
+            valueFromStore += 980000;
+        }
+        return valueFromStore;
+    }
+    public static void WhereisBetterToBuy(double [] PricesHomencenter, double [] PricesDownTown, double [] PricesNeihborghood, int x,String [] NameOfMaterial){
+        for(int i = 0; i<x; i++){
+            if((PricesHomencenter[i]<PricesDownTown[i])&&(PricesHomencenter[i]<PricesNeihborghood[i])){
+                System.out.println("The best place to buy "+NameOfMaterial[i]+" is in Homecenter");
+            }else if((PricesDownTown[i]<PricesHomencenter[i])&&(PricesDownTown[i]<PricesNeihborghood[i])){
+                System.out.println("The best place to buy "+NameOfMaterial[i]+" is in DownTown");
+            }else{
+                System.out.println("The best place to buy "+NameOfMaterial[i]+" is in Neihgborhood Store");
+            }
+        }
+    }
+    public static String [] deployMaterials(Object [] typeObjects,String [] nameOfMaterials, int x, int deploy){
+        String [] array = new String[x];
+        if(deploy == 1){
+            for(int i = 0; i<x ;i++){
+                if(typeObjects[i]==TypeOfWork.WHITE){
+                    array[i] = nameOfMaterials[i];
+                }
+            }
+        }else if(deploy == 2){
+            for(int j = 0; j<x ;j++){
+                if(typeObjects[j]==TypeOfWork.BLACK){
+                    array[j] = nameOfMaterials[j];
+                }
+            }
+        }else{
+            for(int k = 0; k<x ;k++){
+                if(typeObjects[k]==TypeOfWork.WHITE){
+                    array[k] = nameOfMaterials[k];
+                }
+            }
+        }
+        return array;
     }
 }
